54b93699ad92948fa7085dd2b9de6161bdad0dc8
Yehor <egorkorsun7@gmail.com>
2024-06-15 01:16:49 +0200

    block component refactor

--- src/Components/UI/Block/Block.jsx
+++ src/Components/UI/Block/Block.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import style from "./style.module.css";
 
-export default function Block({children, mode}) {
+export default function Block({children, mode,display, justifyContent,alignItems,padding, flex_dir}) {
   return (
     <div className={style.content_block} style={{
-        backgroundColor : mode === "dark"? "#020617" : "#111828"
+        backgroundColor : mode === "dark"? "#020617" : "#111828",
+        display : display,
+        padding:padding,
+        justifyContent : justifyContent,
+        alignItems : alignItems,
+        flexDirection : flex_dir === "column"? "column" : ""
     }}>
       {children}
     </div>
