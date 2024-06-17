f57d52757bd1b510df1ada0e307b17a6810e1a0e
Yehor <egorkorsun7@gmail.com>
2024-06-17 15:53:22 +0200

    Resume page started

---
+++ src/ContentBlocks/Resume/Resume.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from "./style.module.css";
+import Block from '../../Components/UI/Block/Block';
+export default function Resume() {
+  return (
+    <Block>
+      
+    </Block>
+  )
+}
