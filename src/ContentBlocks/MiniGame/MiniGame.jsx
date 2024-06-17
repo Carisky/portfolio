9c6020508cbcf24501fe781cb99024850fccbe1f
Yehor <egorkorsun7@gmail.com>
2024-06-17 15:52:53 +0200

    minigame TicTacToe added

---
+++ src/ContentBlocks/MiniGame/MiniGame.jsx
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import Block from '../../Components/UI/Block/Block';
+import style from "./style.module.css"
+function Square({ value, onSquareClick }) {
+  return (
+    <div className={style.square} onClick={onSquareClick}>
+      {value}
+    </div>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return (
+    <>
+      <div className={style.status}>{status}</div>
+      <div className={style.board_row}>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className={style.board_row}>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className={style.board_row}>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+export default function MiniGame() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handlePlay(nextSquares) {
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  function restartGame() {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
+  }
+
+  return (
+    <Block>
+      <div id="minigame" className={style.title}>
+        {'Just Tic Tac Toe, i mean why not?)'}
+      </div>
+      <div className={style.game}>
+        <div className={style.game_board}>
+          <Board xIsNext={xIsNext} squares={squares} onPlay={handlePlay} />
+        </div>
+        <div className={style.game_info}>
+          <div onClick={restartGame}><ion-icon name="refresh-outline"></ion-icon></div>
+        </div>
+      </div>
+    </Block>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
