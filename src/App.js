28297446adb8c82fe3301ae303f799c8df90ef2b
Yehor <egorkorsun7@gmail.com>
2024-06-17 15:53:07 +0200

    minigame added to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./Components/UI/Footer/Footer";
 import Header from "./Components/UI/Header/Header";
 import About from "./ContentBlocks/About/About";
 import Home from "./ContentBlocks/Home/Home";
+import MiniGame from "./ContentBlocks/MiniGame/MiniGame";
 import Service from "./ContentBlocks/Services/Service";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Home/>
         <About/>
         <Service/>
+        <MiniGame/>
       <Footer />
     </>
   );
